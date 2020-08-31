8db14501a62607ffc428a82332e157ca0dd38c4e
AmitNirGit <67857460+AmitNirGit@users.noreply.github.com>
2020-08-31 17:19:10 +0300

    removed comments

--- client/src/components/ControlPanel.js
+++ client/src/components/ControlPanel.js
@@ -5,9 +5,6 @@ import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
-//import SearchIcon from '@material-ui/icons/SearchIcon';
-import InputAdornment from '@material-ui/core/InputAdornment';
-//import AccountCircle from '@material-ui/icons/AccountCircle';
 
 const ControlPanel = (props) => (
   <form className='form'>
